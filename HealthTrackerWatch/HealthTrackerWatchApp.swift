273127d29858c709e6aa14ccc089340f57421044
Tatiana Mccray <ejamison06@gmail.com>
2026-04-23 19:19:19 -0500

    Initial Commit

---
+++ HealthTrackerWatch/HealthTrackerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthTrackerWatchApp.swift
+//  HealthTrackerWatch
+//
+//  Created by Tatiana6mo on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthTrackerWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
